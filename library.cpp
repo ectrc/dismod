7b94d732c28c232f4be63e682d85063c5954fe79
Frederik <eccntric@retrac.site>
2026-02-03 00:44:22 +0000

    npc's actually have brains now

--- library.cpp
+++ library.cpp
@@ -7,20 +7,8 @@
 #include "hooks/engine/static_construct_object.h"
 #include "hooks/engine/spawn_actor_from_tweaks.h"
 #include "hooks/engine/spawn_actor.h"
-#include "hooks/engine/engine_tick.h"
 
-#include "hooks/dishonored/tick_brain.h"
-#include "hooks/dishonored/create_npc_pawn.h"
-#include "hooks/dishonored/init_npc.h"
-
-#include "hooks/dishonored/components/ai_monitor.h"
-#include "hooks/dishonored/components/ai_knowledge.h"
-#include "hooks/dishonored/components/init_sterring.h"
-#include "hooks/dishonored/components/look_at.h"
-
-#include "engine/engine.h"
 #include "engine/state.h"
-#include "engine/finder.h"
 
 #include "mods/spawn.h"
 
@@ -42,22 +30,12 @@ auto __stdcall thread(void* module) -> void {
     load_package_async_hook::instance()->hook_.enable();
     static_load_object_hook::instance()->hook_.enable();
     static_construct_object_hook::instance()->hook_.enable();
-    spawn_actor_from_tweaks_hook::instance()->hook_.enable();
     spawn_actor_hook::instance()->hook_.enable();
-    init_npc_hook::instance()->hook_.enable();
-    tick_brain_hook::instance()->hook_.enable();
+    spawn_actor_from_tweaks_hook::instance()->hook_.enable();
 
-    ai_monitor_init_hook::instance()->hook_.enable();
-    add_ai_knowledge_to_component_manager_hook::instance()->hook_.enable();
-    init_steering_hook::instance()->hook_.enable();
-    look_at_comp_hook::instance()->hook_.enable();
     init_brain_hook::instance()->hook_.enable();
+    mods::spawn_test_pawn();
 
-    get_default_object_hook::instance()->hook_.enable();
-    get_ai_behaviour_tweak_for_slot_hook::instance()->hook_.enable();
-    init_stim_hook::instance()->hook_.enable();
-
-    mods::spawn_test_pawn(); // blocks
     Sleep(20000);
   }
 
@@ -70,20 +48,10 @@ void __stdcall unload(void* module) {
   load_package_async_hook::instance()->hook_.disable();
   static_load_object_hook::instance()->hook_.disable();
   static_construct_object_hook::instance()->hook_.disable();
-  spawn_actor_from_tweaks_hook::instance()->hook_.disable();
   spawn_actor_hook::instance()->hook_.disable();
-  init_npc_hook::instance()->hook_.disable();
-  tick_brain_hook::instance()->hook_.disable();
+  spawn_actor_from_tweaks_hook::instance()->hook_.disable();
 
-  ai_monitor_init_hook::instance()->hook_.disable();
-  add_ai_knowledge_to_component_manager_hook::instance()->hook_.disable();
-  init_steering_hook::instance()->hook_.disable();
-  look_at_comp_hook::instance()->hook_.disable();
   init_brain_hook::instance()->hook_.disable();
-
-  get_default_object_hook::instance()->hook_.disable();
-  get_ai_behaviour_tweak_for_slot_hook::instance()->hook_.disable();
-  init_stim_hook::instance()->hook_.disable();
 
   LOG("Unloaded!");
 }
--- src/mods/spawn.cpp
+++ src/mods/spawn.cpp
@@ -1,15 +1,9 @@
+#include <initializer_list>
+
 #include "spawn.h"
 
 #include "engine/engine.h"
-#include "logger.h"
-#include "dishonored.h"
-
-#include "hooks/dishonored/components/ai_monitor.h"
-#include "hooks/dishonored/components/ai_knowledge.h"
-#include "hooks/dishonored/init_npc.h"
-#include "hooks/dishonored/tick_brain.h"
-#include "hooks/dishonored/components/init_sterring.h"
-#include "hooks/dishonored/components/look_at.h"
+#include "logger.h"t ad
 
 auto mods::spawn_test_pawn() -> void {
   while (GetAsyncKeyState(VK_INSERT) == 0) { Sleep(100); }
@@ -33,33 +27,20 @@ auto mods::spawn_test_pawn() -> void {
   }
   LOG("tweaks_base brain tweak {}", tweaks_base->m_pBrainTweak == nullptr ? "is null" : tweaks_base->m_pBrainTweak->GetFullName());
 
-  const auto controller_calss = ADishonoredNPCController::StaticClass();
-  LOG("controller_calss {}", controller_calss->GetFullName());
+  const auto ai_tweaks_base = engine::LoadObject<UDisTweaks_AIBrain>(nullptr, L"AI_BrainTweaks_Guard.BrainTweaks_Guard", nullptr, engine::load_flags::memory_reader, nullptr);
+  if (ai_tweaks_base == nullptr) {
+    LOG("Failed to load ai_tweaks_base!");
+    return;
+  }
+  LOG("ai_tweaks_base brain tweak {}", ai_tweaks_base == nullptr ? "is null" : ai_tweaks_base->GetFullName());
+  tweaks_base->m_pBrainTweak = ai_tweaks_base;
 
-  const auto spawned_controller = reinterpret_cast<ADishonoredNPCController*>(engine::spawn_actor(world, controller_calss));
-  const auto controller = reinterpret_cast<ADishonoredNPCController*>(spawned_controller);
+  const auto controller = reinterpret_cast<ADishonoredNPCController*>(engine::spawn_actor(world, ADishonoredNPCController::StaticClass()));
   if (!controller) {
     LOG("Failed to spawn controller!");
     return;
   }
   LOG("spawned controller: {} {}", controller->Location.ToString(), controller->GetFullName());
-
-  const auto brain_spawned = reinterpret_cast<UDishonoredAIBrain *>(engine::StaticConstructObject(tweaks_base->m_pSpawnedObjectClass, controller, "AIBrain"));
-  if (!brain_spawned) {
-    LOG("Failed to brain_spawned!");
-    return;
-  }
-  LOG("brain_spawned: {}", brain_spawned->GetFullName());
-
-  const auto ark_container = reinterpret_cast<UArkComponentContainer *>(engine::StaticConstructObject(UArkComponentContainer::StaticClass(), brain_spawned));
-  if (!ark_container) {
-    LOG("Failed to ark_container!");
-    return;
-  }
-  LOG("ark_container: {}", ark_container->GetFullName());
-
-  brain_spawned->m_pBrainComponentContainer = ark_container;
-  controller->m_pAIBrain = brain_spawned;
 
   const auto actor = reinterpret_cast<ADishonoredNPCPawn*>(engine::spawn_actor_by_tweaks(tweaks_base, EeDisTweaksSpawnType::eDisTweaksSpawnType_InGame, 0, nullptr, nullptr, nullptr, 0, 1, controller));
   if (actor == nullptr) {
@@ -67,87 +48,30 @@ auto mods::spawn_test_pawn() -> void {
     return;
   }
 
+  const auto brain_spawned = reinterpret_cast<UDishonoredAIBrain *>(engine::StaticConstructObject(ai_tweaks_base->m_pSpawnedObjectClass, controller, "AIBrain"));
+  if (!brain_spawned) {
+    LOG("Failed to brain_spawned!");
+    return;
+  }
+  LOG("brain_spawned: {}", brain_spawned->GetFullName());
+  const auto ark_container = reinterpret_cast<UArkComponentContainer *>(engine::StaticConstructObject(UArkComponentContainer::StaticClass(), brain_spawned));
+  if (!ark_container) {
+    LOG("Failed to ark_container!");
+    return;
+  }
+  brain_spawned->m_pBrainComponentContainer = ark_container;
+
+  actor->m_SpawnerInfo = FDisSpawnerInfo{};
+  actor->m_SpawnerInfo.m_Position = {};
+  actor->m_SpawnerInfo.m_Position.m_Loc = actor->Location;
+  actor->m_SpawnerInfo.m_Position.m_Rot = actor->Rotation;
+
+  actor->Controller = controller;
+  actor->m_NPCID = world_info->m_NextNPCID;
+  world_info->m_NextNPCID++;
+
   controller->Possess(actor);
-  LOG("possessed actor: {}", actor->GetFullName());
-  Sleep(200);
-  LOG("brain initialised: {}", actor->GetFullName());
+  controller->m_pAIBrain = brain_spawned;
 
-  brain_spawned->m_pOwningController = controller;
-  brain_spawned->m_pOwningPawn = actor;
-  brain_spawned->m_Home = { .m_Loc = controller->Location, .m_Rot = controller->Rotation };
-  brain_spawned->m_BrainFlags[1] = 1;
-  brain_spawned->m_pBrainComponentContainer->Owner = controller;
-  brain_spawned->m_PendingSuspicionLevel = EDisAISuspicionLevel::DAISL_Unsuspecting;
-  brain_spawned->m_SuspicionLevel = EDisAISuspicionLevel::DAISL_Unsuspecting;
-
-  brain_spawned->m_pGlobalAIManager = world_info->m_pGlobalAIManager;
-  if (brain_spawned->m_pGlobalAIManager->m_pBrainList) brain_spawned->m_pGlobalAI_NextBrain = brain_spawned->m_pGlobalAIManager->m_pBrainList;
-  brain_spawned->m_pGlobalAIManager->m_pBrainList = brain_spawned;
-  brain_spawned->m_pStimManager = brain_spawned->m_pGlobalAIManager->m_pStimManager;
-  brain_spawned->m_LookAtRequest = { };
-
-  FDisAIMonitorReaction monitor_reaction_component = {};
-  monitor_reaction_component.m_pOwningBrain = brain_spawned;
-  monitor_reaction_component.m_pOwner = (uintptr_t)controller;
-  brain_spawned->m_pBrainComponentContainer->m_Components.push_back(FPointer{reinterpret_cast<uintptr_t>(&monitor_reaction_component)});
-  add_ai_knowledge_to_component_manager_hook::instance()->hook_.original()(brain_spawned->m_pBrainComponentContainer);
-
-  brain_spawned->m_pCurrentBehavior = nullptr;
-  brain_spawned->m_BehaviorArray.clear();
-
-  {
-    const auto behave_tweak = (UDisTweaks_AIBehavior_Search*)get_default_object_hook::instance()->hook_.original()(UDisTweaks_AIBehavior_Search::StaticClass(), false);
-    if (!behave_tweak) {
-      LOG("Failed to behave_tweak!");
-      return;
-    }
-    LOG("behave_tweak: {}", behave_tweak->GetFullName());
-
-    const auto behave_obj = reinterpret_cast<UDishonoredAIBehavior*>(engine::StaticConstructObject(UDishonoredAIBehavior::StaticClass(), brain_spawned));
-    if (!behave_obj) {
-      LOG("Failed to behave!");
-      return;
-    }
-    LOG("behave: {}", behave_obj->GetFullName());
-
-    behave_obj->m_pOwningBrain = brain_spawned;
-    behave_obj->m_pBehaviorTweaks = behave_tweak;
-    behave_obj->m_pGlobalAIMan = brain_spawned->m_pGlobalAIManager;
-
-
-    brain_spawned->m_BehaviorArray.push_back(behave_obj);
-  }
-
-  const auto ai_brain_tweaks = (UDisTweaks_AIBrain*)get_default_object_hook::instance()->hook_.original()(UDisTweaks_AIBrain::StaticClass(), false);
-  if (!ai_brain_tweaks) {
-    LOG("Failed to ai_brain_tweaks!");
-    return;
-  }
-  tweaks_base->m_pBrainTweak = ai_brain_tweaks;
-  LOG("ai_brain_tweaks: {}, length of its behaviours: {}", ai_brain_tweaks->GetFullName(), ai_brain_tweaks->m_BehaviorTweak.size());
-
-  FAIStimStruct_BrainInit ai_brain_stim = { };
-  ai_brain_stim.m_StimID = 11;
-  ai_brain_stim.m_pStimManager = brain_spawned->m_pStimManager;
-  ai_brain_stim.m_pSource = brain_spawned;
-  ai_brain_stim.m_VTable_Pointer_Dummy.Dummy = 0xD33938;
-  init_stim_hook::instance()->hook_.original()(&ai_brain_stim);
-
-  FDisStimRef ref = {};
-  ref.m_pStim = FPointer{(uintptr_t)&ai_brain_stim};
-  ref.m_fDelayTimer = 1.0f;
-  ai_brain_stim.m_RefCount++;
-  brain_spawned->m_StimQueue.push_back(ref);
-
-  tick_brain_hook::instance()->hook_.original()(brain_spawned, 0.0f);
-
-  if (actor->Controller == nullptr) return LOG("actor controller is null :(");
-  const auto brain = reinterpret_cast<ADishonoredNPCController*>(actor->Controller)->m_pAIBrain;
-  if (!brain) {
-    LOG("BRAIN IS NOT INITALISED!!!");
-    return;
-  }
-  LOG("Brain: {}", brain->GetFullName());
-
-  // brain->m_bBrainIsInitialized = true;
+  init_brain_hook::instance()->hook_.original()(brain_spawned, actor, ai_tweaks_base, EDisAISuspicionLevel::DAISL_Unsuspecting);
 }
